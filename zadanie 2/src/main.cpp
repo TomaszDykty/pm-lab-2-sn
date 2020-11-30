649070b3d4a322fcdfb99cd2594c54b25a50f78e
Tomasz <tomasz.dykty@student.wat.edu.pl>
2020-11-30 20:21:14 +0100

    zadanie-2-2-4

--- zadanie 2/src/main.cpp
+++ zadanie 2/src/main.cpp
@@ -52,7 +52,44 @@ ISR(INTO_vect){
   digitalWrite(12, state);
 }
 */
+//224
+#include <avr/io.h>
+#define SOLUTION 0
+uint8_t button;
+bool state = true;
+void togglePinD13(bool state) { 
+  PORTB = (state << 5);
+  state = !(state);
+}
 
+void delay() {
+  for (uint32_t j = 0x1FFFF; j > 0; j--)
+    asm volatile("nop");
+}
+
+int main() 
+{
+  DDRB &= !(1 << 0); 
+  DDRB |= (1 << 5);
+  while (1)
+{
+#if (SOLUTION == 0)
+  button = (PINB & (1 << PINB0));
+  if (button == 0)
+  {
+    togglePinD13(&state);
+    delay();
+  }
+  else
+    delay();
+#elif (SOLUTION == 1)
+  while (!(PINB & (1 << PINB0))){
+  togglePinD13(&state);
+  delay();
+}
+#endif
+}
+}
 
 
 
@@ -60,45 +97,6 @@ ISR(INTO_vect){
 
 
 /*
-//224
-#inculde <avr/io.h>
-uint8_t button;
-bool state = true;
-void togglePinD13 (bool *state)
-{
-  PORTB = (*state << 5);
-  *state = !(*state);
-}
-void delay()
-{
-  for (uint32_t j = 0x1FFFF; j > 0 ; j--)
-  {  
-    __asm__ __volatile__("nop");
-  }
-}
-int main()
-{
-  DDRB &= !(1 << 0);
-  DDRB |= (1 << 5);
-  while (1)
-  {
-    button = (PINB & (1<<PINB0));
-    if (button == 0)
-    {
-      togglePinD13(&state);
-    }
-    else
-    {
-        delay();
-    }
-    
-    
-  }
-  
-}
-
-*/
-
 //223 
 #include <avr/io.h>
 
@@ -142,7 +140,7 @@ int main()
     }
   }
 }
-
+*/
 
 
 
